16c7affc89d372a4f3a8140da5adbde14c703289
Mike Dunston <m_dunston@comcast.net>
2021-01-08 06:02:34 -0800

    :pencil: Switch to timestamp for task monitor

--- components/TaskMonitor/FreeRTOSTaskMonitor.cpp
+++ components/TaskMonitor/FreeRTOSTaskMonitor.cpp
@@ -18,6 +18,7 @@ COPYRIGHT (c) 2019-2021 Mike Dunston
 #include "FreeRTOSTaskMonitor.h"
 
 #include <algorithm>
+#include <esp_log.h>
 #include <freertos/task.h>
 #include <soc/soc.h>
 
@@ -40,17 +41,20 @@ FreeRTOSTaskMonitor::FreeRTOSTaskMonitor(Service *service)
 StateFlowBase::Action FreeRTOSTaskMonitor::report()
 {
   UBaseType_t taskCount = uxTaskGetNumberOfTasks();
-  LOG(INFO,
-      "[TaskMon] uptime: %02d:%02d:%02d freeHeap: %u, largest free block: %u, "
-      "tasks: %d, mainBufferPool: %.2fkB"
-    , (uint32_t)(USEC_TO_SEC(esp_timer_get_time()) / 3600)
-    , (uint32_t)(USEC_TO_SEC(esp_timer_get_time()) % 3600) / 60
-    , (uint32_t)(USEC_TO_SEC(esp_timer_get_time()) % 60 )
-    , heap_caps_get_free_size(MALLOC_CAP_INTERNAL)
-    , heap_caps_get_largest_free_block(MALLOC_CAP_8BIT)
-    , taskCount
-    , mainBufferPool->total_size() / 1024.0f
-  );
+  LOG(INFO, "%s: free heap: %.2fkB (max block size: %.2fkB), "
+#if CONFIG_SPIRAM_SUPPORT
+            "Free PSRAM: %.2fkB (max block size: %.2fkB), "
+#endif // CONFIG_SPIRAM_SUPPORT
+            "mainBufferPool: %.2fkB, task count: %d"
+          , esp_log_system_timestamp()
+          , heap_caps_get_free_size(MALLOC_CAP_INTERNAL) / 1024.0f
+          , heap_caps_get_largest_free_block(MALLOC_CAP_INTERNAL) / 1024.0f
+#if CONFIG_SPIRAM_SUPPORT
+          , heap_caps_get_free_size(MALLOC_CAP_SPIRAM) / 1024.0f
+          , heap_caps_get_largest_free_block(MALLOC_CAP_SPIRAM) / 1024.0f
+#endif // CONFIG_SPIRAM_SUPPORT
+          , mainBufferPool->total_size() / 1024.0f, taskCount);
+
 #ifdef CONFIG_TASK_LIST_REPORT
   vector<TaskStatus_t> taskList;
   uint64_t now = esp_timer_get_time();
@@ -72,7 +76,7 @@ StateFlowBase::Action FreeRTOSTaskMonitor::report()
     for (int task = 0; task < retrievedTaskCount; task++)
     {
       LOG(INFO,
-          "[TaskMon] %-16s id:%3d, priority:%2d/%2d, free stack:%5d, core:%4s, "
+          "%-16s id:%3d, priority:%2d/%2d, free stack:%5d, core:%4s, "
           "cpu%%:%-3d, state:%s"
         , taskList[task].pcTaskName
         , taskList[task].xTaskNumber
